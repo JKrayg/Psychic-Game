d6d24d4b7e0ebdb8d3439d1738aee2e0146f5771
Jake Krayger <jkrayg@protonmail.com>
2020-05-04 14:10:35 -0400

    Update to not look so ugly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var allLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guessesleft = 9;
 var badGuess = [];
+
 
 var computerGuess = allLetters[Math.floor(Math.random() * allLetters.length)];
 console.log(computerGuess);
@@ -13,35 +14,38 @@ function reset() {
     guessesleft = 9;
     badGuess = [];
     console.log(computerGuess);
-        
+
 }
 
 
-document.onkeyup = function(event) {
-var userGuess = event.key;
+document.onkeyup = function (event) {
+    var userGuess = event.key;
 
-if (userGuess == computerGuess) {
-    wins++;
-    reset()
-    guessesleft++;
+    if (userGuess === computerGuess) {
+        wins++;
+        reset()
 
-} if (userGuess != computerGuess) {
-    guessesleft--;
+    } else {
+        guessesleft--;
 
-} if (guessesleft === 0) {
-    losses++;
-    reset()
+        if (guessesleft === 0) {
+            losses++;
+            reset()
 
-} else if (badGuess.indexOf(userGuess) === - 1) {
-    badGuess.push(userGuess);
+        } else if (badGuess.indexOf(userGuess) == - 1) {
+            badGuess.push(userGuess);
+            console.log(badGuess)
 
-} else {
-    guessesleft++;
-}
+        } else {
+            guessesleft++;
+        }
+    }
 
-document.getElementById("currentletter").innerHTML = "";
-document.getElementById("userwins").innerHTML = "Wins: " + wins;
-document.getElementById("userlosses").innerHTML  = "Losses: " + losses;
-document.getElementById("guessesremaining").innerHTML = "Guesses left: " + guessesleft;
-document.getElementById("playerguesses").innerHTML  = "Your guesses so far: " + badGuess;
+
+
+    document.getElementById("currentletter").innerHTML = "";
+    document.getElementById("userwins").innerHTML = "Wins: " + wins;
+    document.getElementById("userlosses").innerHTML = "Losses: " + losses;
+    document.getElementById("guessesremaining").innerHTML = "Guesses left: " + guessesleft;
+    document.getElementById("playerguesses").innerHTML = "Your guesses so far: " + badGuess;
 }
